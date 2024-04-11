6440c0ca7af3fa60124fc47486373340a6566e77
shubh1712 <shubhhhhhj17@gmail.com>
2024-04-11 23:32:47 +0530

    login updated

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -38,7 +38,7 @@ function Main() {
           <div className="centered-div1">
             <img src={emailImage} style={{width:"60%",height:"60%"}}alt="" />
             <p>Please Verify your email address. We have sent a confirmation email to your registered email ID</p>
-            <p style={{fontWeight:"bolder",fontSize:"1.2rem"}}>example@gmail.com</p>
+            <p style={{fontWeight:"bolder",fontSize:"1.2rem"}}>{email}</p>
             <p>Click in the link in email to continue using Dribble</p>
             <p>Didnt receive the mail check your spam folder,it may have been caught by  filter.If </p>
             <p>you still dont see it , <button className="btn btn-pink" style={{color:"#ff92a5"}} onClick={handleResend}>resend the confirmation email</button></p>
